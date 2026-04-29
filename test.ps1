667bc853539c1940f1fbac829324add941055add
Guillermo Gasca <80438388+LuisGasca10@users.noreply.github.com>
2026-04-29 09:01:00 -0600

    First Commit

---
+++ test.ps1
@@ -0,0 +1,8 @@
+# mi_herramienta.ps1
+Write-Host "=====================================" -ForegroundColor Cyan
+Write-Host "¡Sistema inicializado por el Profesor!" -ForegroundColor Green
+Write-Host "=====================================" -ForegroundColor Cyan
+
+# Aquí iría tu lógica real: instalar programas, configurar red, etc.
+$usuario = $env:USERNAME
+Write-Host "Hola, $usuario. Tu entorno está listo."
